66d48109a790d16aa0de04f518236ad2daa06a75
Marulanda921 <Alejandromarulanda702@gmail.com>
2024-10-11 10:54:43 -0700

    05

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { 
-    path: 'spline-viewer', 
-    loadChildren: () => import('./Components/spline-viewer/spline-viewer.component').then(m => m.SplineViewerComponent) 
-  }
+
 ];
 
 @NgModule({
